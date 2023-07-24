385aef69e33410def37c097da6ea48f04b088cf4
Your Name <you@example.com>
2023-07-24 08:28:51 +0000

    test

--- serverless/s3.tf
+++ serverless/s3.tf
@@ -68,3 +68,134 @@ resource "aws_s3_bucket_website_configuration" "website_bucket" {
     suffix = "index.html"
   }
 }
+
+
+
+######## cloudfront######
+
+resource "aws_cloudfront_origin_access_identity" "example" {
+  comment = "Some comment"
+}
+
+
+resource "aws_cloudfront_distribution" "cdn_static_site" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  comment             = "my cloudfront in front of the s3 bucket"
+
+  origin {
+    domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id                = "my-s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+  }
+
+  default_cache_behavior {
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "my-s3-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+  aliases = ["event.toydream.shop"]
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    acm_certificate_arn      = aws_acm_certificate.acm_certificate.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "cloudfront OAC"
+  description                       = "description of OAC"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.cdn_static_site.domain_name
+}
+
+
+
+
+
+########## ACM, Route53
+
+# request public certificates from the amazon certificate manager.
+resource "aws_acm_certificate" "acm_certificate" {
+  domain_name               = "*.toydream.shop"
+#  subject_alternative_names = ["changhoon.shop"]
+  validation_method         = "DNS"
+  provider = aws.virginia
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# get details about a route 53 hosted zone
+data "aws_route53_zone" "route53_zone" {
+  zone_id      = "Z10449893AKP9L3IDXBVR"
+#  name         = "changhoon.shop"
+  private_zone = false
+}
+
+# create a record set in route 53 for domain validatation
+resource "aws_route53_record" "route53_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.route53_zone.zone_id
+}
+
+
+## validate acm certificates
+resource "aws_acm_certificate_validation" "acm_certificate_validation" {
+  provider = aws.virginia
+  certificate_arn         = aws_acm_certificate.acm_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.route53_record : record.fqdn]
+}
+
+resource "aws_route53_record" "blog" {
+  zone_id = data.aws_route53_zone.route53_zone.zone_id
+  name    = "event.toydream.shop"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn_static_site.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn_static_site.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
