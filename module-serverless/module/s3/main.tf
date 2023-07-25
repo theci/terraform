c14827c494ce4960dba03407c4e340f63f44adb8
Your Name <you@example.com>
2023-07-25 10:59:00 +0900

    test

--- module-serverless/module/s3/main.tf
+++ module-serverless/module/s3/main.tf
@@ -145,3 +145,71 @@ resource "aws_cloudfront_origin_access_control" "default" {
   signing_protocol                  = "sigv4"
 }
 
+########## ACM, Route53
+
+provider "aws" {
+  alias = "virginia"
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      project = "serverless-demo",
+      type    = "web",
+      iac     = "terraform"
+    }
+  }
+}
+
+# request public certificates from the amazon certificate manager.
+resource "aws_acm_certificate" "acm_certificate" {
+  domain_name               = "*.toydream.shop"
+  validation_method         = "DNS"
+  provider = aws.virginia
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# get details about a route 53 hosted zone
+data "aws_route53_zone" "route53_zone" {
+  zone_id      = "Z10449893AKP9L3IDXBVR"
+  private_zone = false
+}
+
+# create a record set in route 53 for domain validatation
+resource "aws_route53_record" "route53_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.route53_zone.zone_id
+}
+
+
+## validate acm certificates
+resource "aws_acm_certificate_validation" "acm_certificate_validation" {
+  provider = aws.virginia
+  certificate_arn         = aws_acm_certificate.acm_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.route53_record : record.fqdn]
+}
+
+resource "aws_route53_record" "blog" {
+  zone_id = data.aws_route53_zone.route53_zone.zone_id
+  name    = "event.toydream.shop"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn_static_site.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn_static_site.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
