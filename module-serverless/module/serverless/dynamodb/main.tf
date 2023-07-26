36f97242625fd9929f6ff1a02095795fc24ca7cf
peach <node-gitlab-jenkins-ecr@example.com>
2023-07-26 10:52:58 +0900

    test

--- module-serverless/module/serverless/dynamodb/main.tf
+++ module-serverless/module/serverless/dynamodb/main.tf
@@ -1,16 +1,2 @@
-terraform {
-  cloud {
-    organization = "final_project"
-    hostname = "app.terraform.io"
 
-    workspaces {
-      name = "serverless"
-    }
-  }
-}
-
-provider "aws" {
-##  profile = "default"
-  region  = "ap-northeast-2"
-}
 
